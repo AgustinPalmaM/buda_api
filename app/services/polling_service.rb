5d78221cf948d6854bcdfcd5333b5564108ce5a8
AgustinPalmaM <palma.agustin7@gmail.com>
2024-02-13 17:37:33 -0300

    Add test for PollingService

--- app/services/polling_service.rb
+++ app/services/polling_service.rb
@@ -5,24 +5,37 @@ class PollingService
 
 
   def polling(market)
-    return nil unless Api::V1::Market.permitted?(market)
+    return nil unless permitted?(market)
 
     current_spread = SpreadService.new.find_spread(market)
     spread_alert = find_alert(market).to_f
 
-    message = set_message(current_spread, spread_alert)
+    message = set_message(current_spread.to_f, spread_alert.to_f)
 
     { current_spread:, spread_alert:, message: }
   end
 
   def find_alert(market)
+    return nil unless permitted?(market)
+    
     alerts = AlertService.load_alerts
     target_alert = alerts.find { |alert| alert['market'] == market }
-    target_alert['alert_spread'] unless target_alert.nil?
+    target_alert['alert_spread'].to_f unless target_alert.nil?
   end
 
   def set_message(current_spread, spread_alert)
-    if spread_alert.nil?
+
+    unless current_spread.is_a?(Numeric) || current_spread.nil?
+      return nil
+    end
+  
+    unless spread_alert.is_a?(Numeric) || spread_alert.nil?
+      return nil
+    end
+    
+    if current_spread.nil?
+      "there is not current spread available"
+    elsif spread_alert.nil?
       'there is not saved alert spread'
     elsif current_spread > spread_alert
       'current spread is greater than saved alert'
@@ -31,5 +44,9 @@ class PollingService
     else
       'current spread and saved alert are the same price'
     end
+  end
+
+  def permitted?(market)
+    Api::V1::Market.permitted?(market)
   end
 end
--- test/services/polling_service_test.rb
+++ test/services/polling_service_test.rb
@@ -37,6 +37,43 @@ class PollingServiceTest < ActiveSupport::TestCase
     assert_nil PollingService.new.polling(market)
   end
 
+  test "Should return alert value if alert market is saved for the market given" do
+    market = 'btc-clp'
+    alert_target = PollingService.new.find_alert(market)
+    assert alert_target
+    assert_instance_of Float, alert_target
+    assert_equal 9.0, alert_target
+  end
+
+  test "Should return nil when is not saved an alert for the market given" do
+    market = 'btc-cop'
+    alert_target = PollingService.new.find_alert(market)
+    assert_nil alert_target
+  end
+
+  test "Should return nil when pass an invalid market" do
+    market = 'btc-cap'
+    alert_target = PollingService.new.find_alert(market)
+    assert_nil alert_target
+  end
+
+  test "return valid messages when pass numeric or nil params current_spread or spread_alert" do
+    assert_equal "there is not current spread available", PollingService.new.set_message(nil, 1)
+    assert_equal 'there is not saved alert spread', PollingService.new.set_message(1, nil)
+    assert_equal 'current spread is greater than saved alert', PollingService.new.set_message(10,1)
+    assert_equal 'current spread is less than saved alert', PollingService.new.set_message(1,10)
+    assert_equal 'current spread and saved alert are the same price', PollingService.new.set_message(1,1)
+
+  end
+
+  test "return nil when pass not numeric or nil value to params current_spread or spread_alert" do
+    assert_nil PollingService.new.set_message('1', '1')
+    assert_nil PollingService.new.set_message('1', nil)
+    assert_nil PollingService.new.set_message(10, [])
+    assert_nil PollingService.new.set_message('1', [])
+    assert_nil PollingService.new.set_message({}, 1)
+  end
+
 
 
 
